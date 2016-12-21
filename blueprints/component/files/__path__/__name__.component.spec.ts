74c645b031797eb35ef70765dd50a2619cc63a20
Pete Selby <peter.selby2@tiaa.org>
2016-12-20 19:28:38 -0500

    added to component blueprints

--- blueprints/component/files/__path__/__name__.component.spec.ts
+++ blueprints/component/files/__path__/__name__.component.spec.ts
@@ -1,28 +1,54 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { Component, OnInit, DebugElement } from '@angular/core';
 
 import { <%= classifiedModuleName %>Component } from './<%= dasherizedModuleName %>.component';
 
+// Mock service
+class <%= classifiedModuleName %>ServiceMock {
+  // Mock all service calls used in this component
+}
+// Mock hosting component
+// This is a parent component which holds the component we are testing. This
+// allows us to pass in variables from a parent and catch events being thrown.
+@Component({
+  // simple template with just the tested component
+  template: '<<%= selector %>></<%= selector %>>'
+})
+class <%= classifiedModuleName %>HostComponentMock implements OnInit {
+  ngOnInit(): void { }
+}
+
 describe('<%= classifiedModuleName %>Component', () => {
+  let hostComponent: <%= classifiedModuleName %>HostComponentMock;
+  let hostFixture: ComponentFixture<<%= classifiedModuleName %>HostComponentMock>;
   let component: <%= classifiedModuleName %>Component;
   let fixture: ComponentFixture<<%= classifiedModuleName %>Component>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ <%= classifiedModuleName %>Component ]
+	  imports: [],
+      declarations: [ <%= classifiedModuleName %>Component,  <%= classifiedModuleName %>HostComponentMock],
+	  providers: [
+		{provide: '<Some Injected Service Name>', useClass: <%= classifiedModuleName %>ServiceMock}
+	  ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    hostFixture = TestBed.createComponent(<%= classifiedModuleName %>HostComponentMock);
+    hostComponent = hostFixture.componentInstance;
+    hostFixture.detectChanges();
+	
     fixture = TestBed.createComponent(<%= classifiedModuleName %>Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created on init', () => {
+    expect(hostComponent).toBeTruthy();
     expect(component).toBeTruthy();
   });
 });
